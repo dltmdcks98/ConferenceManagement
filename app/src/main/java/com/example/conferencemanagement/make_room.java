b24e1ff328749b7c07569285072b38ba08daced5
dltmdcks98 <dltmdcks98@naver.com>
2021-07-13 21:41:07 +0900

    방 만들기

--- app/src/main/java/com/example/conferencemanagement/make_room.java
+++ app/src/main/java/com/example/conferencemanagement/make_room.java
@@ -3,16 +3,39 @@ package com.example.conferencemanagement;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
 public class make_room extends Activity {
-
+    EditText roomCode,roomName;
+    Button ranbomCode,btnSave;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.make_room);
+
+        roomCode = (EditText)findViewById(R.id.roomCode);
+        roomName = (EditText)findViewById(R.id.roomName);
+        ranbomCode = (Button)findViewById(R.id.ranbomCode);
+        btnSave = (Button)findViewById(R.id.btnSave);
+
+        ranbomCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
