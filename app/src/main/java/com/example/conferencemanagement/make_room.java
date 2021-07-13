0720d3b5793f25fdd508eb0a4daf9543a05e8f7e
dltmdcks98 <dltmdcks98@naver.com>
2021-07-13 21:00:34 +0900

    방 만들기

---
+++ app/src/main/java/com/example/conferencemanagement/make_room.java
@@ -0,0 +1,18 @@
+package com.example.conferencemanagement;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class make_room extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.make_room);
+
+    }
+}
