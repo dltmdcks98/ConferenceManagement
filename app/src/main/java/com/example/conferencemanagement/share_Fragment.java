17f8f3446f52d212e01d772c9da440f0f16ccd52
adorable-Kihwan <qkrrlghks20@naver.com>
2021-07-17 11:46:15 +0900

    Hi

--- app/src/main/java/com/example/conferencemanagement/share_Fragment.java
+++ app/src/main/java/com/example/conferencemanagement/share_Fragment.java
@@ -7,22 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
-public class teamMember_Fragment extends Fragment {
+public class share_Fragment extends Fragment {
 
-    EditText editLimit, editProgress;
-    ImageView imageView;
-    TextView textView;
-    ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_member_, container, false);
+        return inflater.inflate(R.layout.fragment_share, container, false);
     }
 }
